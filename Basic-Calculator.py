b14917f74d543e8d2c75d14497da3b69d1e4e233
hasan-firan <hasan.firan@gmail.com>
2026-08-04 00:22:00 +0300

    Refactor basic calculator input and error handling
    Refactored input handling and error messages for clarity.

--- Basic-Calculator.py
+++ Basic-Calculator.py
@@ -1,8 +1,6 @@
 #בניית פונקציית מחשבון בסיסי שמקבל שני ערכים ומבצע פעולות פשוטות(+,-,/,*)
 
-#a = int(input("select first number:"))
-#b = int(input("select second number:"))
-#op = input("select an operation")
+
 
 def calculator(a,b,op):
     if op == "+":
@@ -20,10 +18,6 @@ def calculator(a,b,op):
         return"select legal operation"
 
 
-#print(calculator(a,b,op))
-
-#אחרי כל פעולת חישוב שמסתיימת בהצלחה יקפוץ למשתמש שיצטרך לענות עליה ובהתאם לכך תיקבע התנהגות התוכנית
-#אני מוודא גם שהמשתמש יכניס קלט נכון (מספר בלבד,כל הזנה אחרת תיחשב לקלט שגוי)
 
 while True:
        try:
@@ -34,49 +28,10 @@ while True:
         result = calculator(a,b,op)
         print("result:",result)
 
-        Continue = input("continue to another order? Y/N: ")
+       except ValueError:
+        print("Invalid input, enter a number")
 
-        Continue = Continue.lower()
-
-        if Continue == "y":
-            a = int(float(input("select first number:")))
-            b = int(float(input("select second number:")))
-            op = input("select an operation:")
-
-            result = calculator(a, b, op)
-
-            print("result:",result)
-
-            Continue = input("continue to another order? Y/N: ")
-
-            Continue = Continue.lower()
-
-        else:
-            print("Thank you for using our service")
+        again = input("Continue to another order> Y/N:").lower()
+        if again != "y":
+            print("Thank you for using our service. Goodbye!")
             break
-
-#כאן מכסים את האופציה לקלט שגוי מצד המשתמש עם אופציה להזדמנות נוספת להזנת קלט נכון
-       except:
-        print("'invalid input' enter a number")
-
-        again = input("Do you want to run another calculation? (yes/no:")
-
-        again = again.lower()
-
-        if again != "yes":
-           print("Goodbye")
-           break
-
-
-
-
-
-
-
-
-
-
-
-
-
-
